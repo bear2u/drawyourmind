3e5b5609dcc9cf9807fac0bebfd39b316509bee6
김태현 <bear2u@naver.com>
2019-07-31 13:56:17 +0900

    Initial commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return LoginState();
+  }
+}
+
+class LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('body'),
+    );
+  }
+}
